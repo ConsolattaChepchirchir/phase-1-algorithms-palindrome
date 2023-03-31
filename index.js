f0f4d42c346e486b549b29eb7533c00e8cc0bf92
ConsolattaChepchirchir <consochep97@gmail.com>
2023-03-31 13:31:01 +0300

    complete palindrome assignment

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let rWord=word.split("").reverse().join("")
+  if(word==rWord){
+    return true;
+  }
+  else{
+    return false;
+  }
+
 }
+console.log(isPalindrome("racecar"))
+
 
 /* 
   Add your pseudocode here
+  declare isPalindrome function
+  for our code if the word in the function is true it will return true
+  else false
 */
 
 /*
   Add written explanation of your solution here
+  I began by declare a variable rWord
+  use split("") to split the word a return them in a array
+  the reverse the word which are split
+  use join("") to join them together
+  if they match with the original it will return true or false
+  based on the output.
 */
 
 // You can run `node index.js` to view these console logs
